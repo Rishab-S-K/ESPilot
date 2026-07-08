04fe88aae973b0ffe237bb2c9931541c68b2842a
Rishab-S-K <rishabsk2@gmail.com>
2026-07-07 23:04:20 -0700

    Updated model config for testing

--- chatbot/tests/test_chat.py
+++ chatbot/tests/test_chat.py
@@ -9,13 +9,22 @@ from google.genai import types
 load_dotenv()
 client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
 
+##Model Decleration
 config = types.GenerateContentConfig(
     tools=[esp_tools],
     temperature=0.7,
     system_instruction=(
-        "You are ESPilot..."  # same system instruction as main.py
+        "You are ESPilot, a chatbot that is able to control a ESP32-controlled breadboard. "
+        "The breadboard has LEDS (red, yellow, blue, green, white), an RGB LED, temperature "
+        "sensor, light sensor, passive buzzer, active buzzer, and a LCD display with 4 rows. "
+        "Interpret user requests and call the appropriate tool to control the breadboard. "
+        "Here are some of the features you may be asked(you must not be limited to these, be creative). "
+        "-Generating your own song. "
+        "-Playing a known song by transcribing the sheet music. Note that only 1 frequency can be played at one time due to there being only 1 passive buzzer. "
+        "-Generating fun color themes for the LEDS, like if you are asked to make the breadboard 'spooky'. "
     )
 )
+
 
 def act(name, args):
     args = dict(args)
